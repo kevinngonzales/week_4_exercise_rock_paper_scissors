ff9d536ce05edd7b4740dfa78bbea83583974124
kevinngonzales <kevinngstem@gmail.com>
2023-11-07 20:16:45 -0800

    added ui interface

--- script.js
+++ script.js
@@ -1,10 +1,67 @@
-//declaring global variables
 let playerWins = 0;
 let computerWins = 0;
 let ties = 0;
 let roundNumber = 0;
+let score = document.getElementById("displayResults")
+let winLossTitle = document.getElementById("title");
 
-// function to get rock, paper, or scissors from computer using math.random()
+let roundCount = document.getElementById("roundCount");
+let tieCount = document.getElementById("tieCount");
+let winCount = document.getElementById("winCount");
+let lossCount = document.getElementById("lossCount");
+
+var newButton1 = document.createElement("button");
+var newButton2 = document.createElement("button");
+var newButton3 = document.createElement("button");
+
+body.appendChild(newButton1);
+newButton1.id = "button1";
+newButton1.innerHTML = "Rock";
+body.appendChild(newButton2);
+newButton2.id = "button2";
+newButton2.innerHTML = "Paper";
+body.appendChild(newButton3);
+newButton3.id = "button3";
+newButton3.innerHTML = "Scissors";
+
+newButton1.style.display = "none";
+newButton2.style.display = "none";
+newButton3.style.display = "none";
+
+var playGameButton = document.createElement("button");
+
+
+body.appendChild(playGameButton);
+playGameButton.id = "playGameButton";
+playGameButton.innerHTML = "Play";
+playGameButton.addEventListener("click", removePlayButton);
+
+newButton1.addEventListener("click", () => playRound("rock"));
+newButton2.addEventListener("click", () => playRound("paper"));
+newButton3.addEventListener("click", () => playRound("scissors"));
+
+function removePlayButton() {
+  roundNumber = 0;
+  ties = 0;
+  playerWins = 0;
+  computerWins = 0;
+  updateScore();
+  title.innerText = " ";
+playGameButton.style.display = "none";
+  newButton1.style.display = "";
+  newButton2.style.display = "";
+  newButton3.style.display = "";
+}
+
+
+function removePlayButtons() {
+  newButton1.style.display = "none";
+  newButton2.style.display = "none";
+  newButton3.style.display = "none";
+  playGameButton.style.display = "";
+}
+
+
 function getComputerChoice() { 
   var randomMove = Math.random();
   if (randomMove < 0.33) {
@@ -16,59 +73,46 @@ function getComputerChoice() {
   }
 }
 
-// function to play a round. prompt user for input
-function playRound() {
+
+function playRound(playerChoice) {
   const computerChoice = getComputerChoice();
-  let playerChoice;
-// used while loop to make sure user enters valid input
-  while (true) {
-    playerChoice = prompt("Rock, Paper, or Scissors?");
-    if (playerChoice === null) {
-      return;
-    } else if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
-      break;
-    } else {
-      alert("Please enter rock, paper, or scissors!");
-      return;
-    }
-  }
-// comparing player choice to computer choice and determining who wins or if it is a tie
-// alert the outcome and round and then log the wins
-// iterate player wins, computer wins, ties, and round number to alert user
   if (playerChoice === computerChoice) {
-    alert("It is a tie!");
+    alert("It was a tie!")
     ties++;
   } else if (
     (playerChoice === "rock" && computerChoice === "scissors") ||
     (playerChoice === "scissors" && computerChoice === "paper") ||
     (playerChoice === "paper" && computerChoice === "rock")
-  ) {
-    alert("You win!");
+  ) { 
+    alert("You won!");
     playerWins++;
   } else {
-    alert("Computer Wins");
+    alert("You lost!");
     computerWins++;
   }
+  console.log(playerChoice);
   roundNumber++;
-  alert(`Round Number: ${roundNumber}, Ties: ${ties} Player: ${playerWins}, Computer: ${computerWins}`);   
+  updateScore();
+  resetGame();
 }
 
-// function to play multiple rounds depending on the wins
-function playGame() {
-// use while loop to continue the game if wins are less than 2
-  while (playerWins < 2 && computerWins < 2) {
-    playRound();
-// if either player wins 2 times break and alert
-    if (playerWins === 2) {
-      alert("You have won!");
-      break;
-    } else if (computerWins === 2) {
-      alert("You lost!");
-      break;
-    } 
-  }  
-}
 
-// call the function to play the game
-playGame();
+function resetGame() {
+  if (playerWins === 5) {
+  title.innerText = "You Win! Play Again?";
+    removePlayButtons();
+  }
+  else if (computerWins === 5) {
+    title.innerText = "You Lost! Play Again?";
+    removePlayButtons();
+    }
+  }
+
+
+function updateScore() {
+  roundCount.innerText = roundNumber;
+  tieCount.innerText = ties;
+  winCount.innerText = playerWins;
+  lossCount.innerText = computerWins;
+}
 
